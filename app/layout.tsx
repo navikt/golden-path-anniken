c8ccec2ee28afe5fb09608fffb97e0f37317efd5
Anniken Granberg <anniken.granberg@na.no>
2026-01-07 09:37:18 +0100

    not found

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,9 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
 import Script from "next/script";
+import "@navikt/ds-css";
 import { fetchDecoratorReact } from "@navikt/nav-dekoratoren-moduler/ssr";
+
 import { Page } from "@navikt/ds-react";
-
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Create Next App",
